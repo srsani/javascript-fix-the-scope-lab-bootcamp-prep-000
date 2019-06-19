a0492e5fe0878349cbe32fbeea553465fd492e17
droll-accelerator-1483 <srsani@gmail.com>
2019-06-19 15:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function yourAnimal(your_animal = "cat") {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = your_animal
   return animal
 }
 
